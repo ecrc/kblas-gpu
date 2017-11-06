41cadc7bcc6823bd459f4675d7d1aa1a8d79dc67
Wajih Halim Boukaram <wajihhalim.boukaram@kaust.edu.sa>
2017-11-06 13:26:19 +0300

    QR interface changes

--- src/batch_svd/batch_permute.h
+++
@@ -1,7 +0,0 @@
-#ifndef __BATCH_PERMUTE_H__
-#define __BATCH_PERMUTE_H__
-
-void kblas_permute_columns_batched(int rows, int cols, float** M_ptrs, int ldm, int** perm, int* rank, int num_ops);
-void kblas_permute_columns_batched(int rows, int cols, double** M_ptrs, int ldm, int** perm, int* rank, int num_ops);
-
-#endif

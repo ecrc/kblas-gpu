25688bc86abf431fb6493a7110cb1b66c89a8340
acharara <ali.charara@kaust.edu.sa>
2017-11-13 23:22:50 +0300

    add testing TRTRI, and more ranges to test

--- kblas-test-batch-parallel.py
+++ kblas-test-batch-parallel.py
@@ -51,6 +51,7 @@ TEST_BATCH_GEMM = 1
 TEST_BATCH_SYRK = 1
 TEST_BATCH_POTRF = 1
 TEST_BATCH_LAUUM = 1
+TEST_BATCH_TRTRI = 1
 
 #--------------------------------
 def task1(pVariants, pRanges, pExec, pOptions, pBatchCount, pDev, pOutfile):
@@ -134,7 +135,10 @@ if (TEST_BATCH_GEMM == 1):
                 'test_cgemm_batch',
                 'test_zgemm_batch'
                 ]
-    ranges = ['--range 32:256:32']
+    ranges = [
+              '--range 2:70+1',
+              '--range 32:256:32'
+              ]
     options = check
     batchCount = defaultBatchCount;
 
@@ -157,7 +161,10 @@ if (TEST_BATCH_TRMM == 1):
                 'test_ctrmm_batch',
                 'test_ztrmm_batch'
                 ]
-    ranges = ['--range 32:256:32']
+    ranges = [
+              '--range 2:70+1',
+              '--range 32:256:32'
+              ]
     options = check
     batchCount = defaultBatchCount;
 
@@ -179,7 +186,10 @@ if (TEST_BATCH_TRSM == 1):
                 # 'test_ctrsm_batch',
                 # 'test_ztrsm_batch'
                 ]
-    ranges = ['--range 32:256:32']
+    ranges = [
+              '--range 2:70+1',
+              '--range 32:256:32'
+              ]
     options = check
     batchCount = defaultBatchCount;
 
@@ -197,7 +207,10 @@ if (TEST_BATCH_SYRK == 1):
                 'test_csyrk_batch',
                 'test_zsyrk_batch'
                 ]
-    ranges = ['--range 32:256:32']
+    ranges = [
+              '--range 2:70+1',
+              '--range 32:256:32'
+              ]
     options = check
     batchCount = defaultBatchCount;
 
@@ -213,13 +226,16 @@ if (TEST_BATCH_POTRF == 1):
                 'test_cpotrf_batch',
                 'test_zpotrf_batch'
                 ]
-    ranges = ['--range 32:256:32']
+    ranges = [
+              '--range 2:70+1',
+              '--range 32:256:32'
+              ]
     options = check
     batchCount = defaultBatchCount;
 
     parallelTaskLaunch(variants, programs, ranges, options, batchCount)
 
-############### BATCH_POTRF
+############### BATCH_LAUUM
 if (TEST_BATCH_LAUUM == 1):
     variants = ['-L',
                 # '-U',
@@ -229,7 +245,31 @@ if (TEST_BATCH_LAUUM == 1):
                 'test_clauum_batch',
                 'test_zlauum_batch'
                 ]
-    ranges = ['--range 32:256:32']
+    ranges = [
+              '--range 2:70+1',
+              '--range 32:256:32'
+              ]
+    options = check
+    batchCount = defaultBatchCount;
+
+    parallelTaskLaunch(variants, programs, ranges, options, batchCount)
+
+############### BATCH_TRTRI
+if (TEST_BATCH_TRTRI == 1):
+    variants = ['-L -DN',
+                # '-L -DU',
+                # '-U -DN',
+                # '-U -DU',
+                ]
+    programs = ['test_strtri_batch',
+                'test_dtrtri_batch',
+                'test_ctrtri_batch',
+                'test_ztrtri_batch'
+                ]
+    ranges = [
+              '--range 2:70+1',
+              '--range 32:256:32'
+              ]
     options = check
     batchCount = defaultBatchCount;
 
